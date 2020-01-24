5efa655e41c927c1de89884cb10845219f3c63bd
Gerard Roche <gerardroche@users.noreply.github.com>
2020-01-24 05:18:57 +0000

    :metal:

---
+++ src/Fizz.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GithubActions;
+
+
+class Fizz
+{
+
+}
---
+++ tests/FizzTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace GithubActions;
+
+class FizzTest extends \PHPUnit\Framework\TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
